6098b19a3c7dfdb59244733b14477d5b06bf9599
nfsakmal <mochamad.119140135@student.itera.ac.id>
2025-08-25 13:59:42 +0700

    Creating Create Post page

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -25,7 +25,10 @@ class PostController extends Controller
      */
     public function create()
     {
-        return view('post.create');
+        $categories = Category::get();
+        return view('post.create', [
+            'categories' => $categories,
+        ]);
     }
 
     /**
@@ -33,7 +36,13 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        dd($request->all());
+        $request->validate([
+            'image' => ['required', 'image', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048'],
+            'title' => 'required',
+            'content' => 'required',
+            'category_id' => ['required', 'exists:categories,id'],
+        ]);
+
     }
 
     /**
--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -2,21 +2,48 @@
     <div class="py-4">
         <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-8">
-                <form action="{{ route('post.store') }}" method="post">
+                <form action="{{ route('post.store') }}" enctype="multipart/form-data" method="post">
+                    @csrf
+                    <!-- Image -->
+                    <div>
+                        <x-input-label for="image" :value="__('Image')" />
+                        <x-text-input id="image" class="block mt-1 w-full" type="file" name="image"
+                            :value="old('image')" required autofocus />
+                        <x-input-error :messages="$errors->get('image')" class="mt-2" />
+                    </div>
+
                     <!-- Title -->
-                    <div>
+                    <div class="mt-4">
                         <x-input-label for="title" :value="__('Title')" />
                         <x-text-input id="title" class="block mt-1 w-full" type="text" name="title"
                             :value="old('title')" required autofocus />
-                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                        <x-input-error :messages="$errors->get('title')" class="mt-2" />
                     </div>
+
+                    <!-- Category -->
+                    <div class="mt-4">
+                        <x-input-label for="category_id" :value="__('Category')" />
+                        <select name="category_id" id="category_id">
+                            <option value="">Select a Category</option>
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->id }}">
+                                    {{ $category->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                        <x-input-error :messages="$errors->get('category_id')" class="mt-2" />
+                    </div>
+
                     <!-- Content -->
-                    <div>
-                        <x-input-label for="title" :value="__('Title')" />
-                        <x-text-input id="title" class="block mt-1 w-full" type="text" name="title"
-                            :value="old('title')" required autofocus />
-                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    <div class="mt-4">
+                        <x-input-label for="content" :value="__('Content')" />
+                        <x-input-textarea id="content" class="block mt-1 w-full" type="text" name="content"
+                            :value="old('content')" required></x-input-textarea>
+                        <x-input-error :messages="$errors->get('content')" class="mt-2" />
                     </div>
+                    <x-primary-button class="mt-4">
+                        Submit
+                    </x-primary-button>
                 </form>
             </div>
         </div>
